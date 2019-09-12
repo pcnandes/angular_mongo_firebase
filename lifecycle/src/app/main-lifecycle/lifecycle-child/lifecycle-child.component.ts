2fa132ad78323b25c75ad938283f4d53e02cbd5c
Paulo <paulo.fernandes@serpro.gov.br>
2019-09-12 17:10:33 -0300

    lifecycle hooks

--- lifecycle/src/app/main-lifecycle/lifecycle-child/lifecycle-child.component.ts
+++ lifecycle/src/app/main-lifecycle/lifecycle-child/lifecycle-child.component.ts
@@ -1,19 +1,43 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy, OnChanges, SimpleChanges, AfterContentInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-lifecycle-child',
   templateUrl: './lifecycle-child.component.html',
   styleUrls: ['./lifecycle-child.component.css']
 })
-export class LifecycleChildComponent implements OnInit {
+export class LifecycleChildComponent implements OnInit, OnDestroy, OnChanges, AfterContentInit, AfterViewInit {
 
   @Input() name: string;
   @Input() age: number;
   @Input() food: string;
 
-  constructor() { }
+  // primeiro a ser chamado, as variaveis/ inputs ainda não estao setadas
+  constructor() {
+    console.log(this.name + ' - constructor');
+  }
 
+  // segundo hook a ser chamado. Aqui os inputs já estao definidos
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(this.name + ' - ngOnChanges');
+  }
+
+  // terceiro hook
   ngOnInit() {
+    console.log(this.name + ' - ngOnInit');
+  }
+
+  // quarto hook
+  ngAfterContentInit() {
+    console.log(this.name + ' - ngAfterContentInit');
+  }
+
+  // quinto hook
+  ngAfterViewInit() {
+    console.log(this.name + ' - ngAfterViewInit');
+  }
+
+  ngOnDestroy() {
+    console.log(this.name + ' - ngOnDestroy');
   }
 
 }
