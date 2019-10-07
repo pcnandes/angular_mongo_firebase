fbd67a49183e7e37119a61957f88911cd5eb378a
Paulo <paulo.fernandes@serpro.gov.br>
2019-10-07 10:52:30 -0300

    inicio do formulario

---
+++ template-driven/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FormsModule } from '@angular/forms';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateDrivenFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    // para usar formulário é preciso importar o forms module
+    FormsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
